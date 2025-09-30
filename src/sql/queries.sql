1be71f2c278152d5bf68151acbd724bc047e1880
AdriARE <adrian.r.estevez@gmail.com>
2025-09-30 18:35:33 +0000

    Proyecto SQL con enunciados hasta el 12

--- src/sql/queries.sql
+++ src/sql/queries.sql
@@ -4,40 +4,48 @@ SELECT * FROM climate;
 SELECT * FROM observations;
 
 
--- MISSION 1;
-SELECT *FROM observations
-LIMIT 10;
+--1) ¿Cuáles son las primeras 10 observaciones registradas?;
+-- Utiliza LIMIT para mostrar solo una parte de la tabla;
+SELECT *FROM observations LIMIT 10;
 
--- MISSION 2 ;
---¿Qué identificadores de región (region_id) aparecen en los datos?;
---Usa SELECT DISTINCT para evitar repeticiones.;
+
+--2) ¿Qué identificadores de región (region_id) aparecen en los datos?;
+--Usa SELECT DISTINCT para evitar repeticiones;
 SELECT DISTINCT region_id FROM observations;
 
 
--- MISSION 3;
---¿Cuántas especies distintas (species_id) se han observado?;
---Combina COUNT con DISTINCT para no contar duplicados.;
+--3) ¿Cuántas especies distintas (species_id) se han observado?;
+-- Combina COUNT con DISTINCT para no contar duplicados;
 SELECT COUNT (DISTINCT species_id) FROM observations;
 
--- MISSION 4;
---¿Cuántas observaciones hay para la región con region_id = 2?;
---Aplica una condición con WHERE.;
+
+--4) ¿Cuántas observaciones hay para la región con region_id = 2?;
+-- Aplica una condición con WHERE.;
 SELECT COUNT (*) FROM observations WHERE region_id = 2;
 
 
--- MISSION 5;
---¿Cuántas observaciones se registraron el día 1998-08-08?;
---Filtra por fecha exacta usando igualdad.;
+--5) ¿Cuántas observaciones se registraron el día 1998-08-08?;
+-- Filtra por fecha exacta usando igualdad.;
 SELECT COUNT (*) FROM observations WHERE observation_date = "1998-08-08";
 
 
--- MISSION 6
--- Your query here;
+--6) ¿Cuál es el region_id con más observaciones?;
+-- Agrupa por región y cuenta cuántas veces aparece cada una;
 
+--7) ¿Cuáles son los 5 species_id más frecuentes?;
+-- Agrupa, ordena por cantidad descendente y limita el resultado;
 
--- MISSION 7
--- Your query here;
+--8) ¿Qué especies (species_id) tienen menos de 5 registros?;
+-- Agrupa por especie y usa HAVING para aplicar una condición;
 
+-- 9) ¿Qué observadores (observer) registraron más observaciones?;
+-- Agrupa por el nombre del observador y cuenta los registros;
 
--- MISSION 8
--- Your query here;
+-- 10) Muestra el nombre de la región (regions.name) para cada observación;
+-- Relaciona observations con regions usando region_id;
+
+-- 11) Muestra el nombre científico de cada especie registrada (species.scientific_name);
+-- Relaciona observations con species usando species_id;
+
+-- 12) ¿Cuál es la especie más observada por cada región?;
+-- Agrupa por región y especie, y ordena por cantidad;
