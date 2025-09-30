57f589b5a5e0f3333c4094824c3dafa4af2109dd
AdriARE <adrian.r.estevez@gmail.com>
2025-09-30 19:43:00 +0000

    Atascado en el 10

--- src/sql/queries.sql
+++ src/sql/queries.sql
@@ -6,27 +6,34 @@ SELECT * FROM observations;
 
 --1) ¿Cuáles son las primeras 10 observaciones registradas?;
 -- Utiliza LIMIT para mostrar solo una parte de la tabla;
-SELECT *FROM observations LIMIT 10;
+SELECT *FROM observations 
+LIMIT 10;
 
 
 --2) ¿Qué identificadores de región (region_id) aparecen en los datos?;
 --Usa SELECT DISTINCT para evitar repeticiones;
-SELECT DISTINCT region_id FROM observations;
+SELECT DISTINCT region_id 
+FROM observations;
 
 
 --3) ¿Cuántas especies distintas (species_id) se han observado?;
 -- Combina COUNT con DISTINCT para no contar duplicados;
-SELECT COUNT (DISTINCT species_id) FROM observations;
+SELECT COUNT (DISTINCT species_id) 
+FROM observations;
 
 
 --4) ¿Cuántas observaciones hay para la región con region_id = 2?;
 -- Aplica una condición con WHERE.;
-SELECT COUNT (*) FROM observations WHERE region_id = 2;
+SELECT COUNT (*) 
+FROM observations 
+WHERE region_id = 2;
 
 
 --5) ¿Cuántas observaciones se registraron el día 1998-08-08?;
 -- Filtra por fecha exacta usando igualdad.;
-SELECT COUNT (*) FROM observations WHERE observation_date = "1998-08-08";
+SELECT COUNT (*) 
+FROM observations 
+WHERE observation_date = "1998-08-08";
 
 
 --6) ¿Cuál es el region_id con más observaciones?;
@@ -34,19 +41,48 @@ SELECT COUNT (*) FROM observations WHERE observation_date = "1998-08-08";
 SELECT region_id, COUNT(*) AS total_observations
 FROM observations
 GROUP BY region_id
-ORDER BY total_observations DESC;
+ORDER BY total_observations DESC
+LIMIT 1;
 
 --7) ¿Cuáles son los 5 species_id más frecuentes?;
 -- Agrupa, ordena por cantidad descendente y limita el resultado;
+SELECT species_id, COUNT(*) AS register_for_specie
+FROM observations
+GROUP BY species_id
+ORDER BY register_for_specie DESC
+LIMIT 5;
+
 
 --8) ¿Qué especies (species_id) tienen menos de 5 registros?;
 -- Agrupa por especie y usa HAVING para aplicar una condición;
+SELECT species_id, COUNT(*) AS register_for_specie
+FROM observations
+GROUP BY species_id
+HAVING register_for_specie < 5;
+
 
 -- 9) ¿Qué observadores (observer) registraron más observaciones?;
 -- Agrupa por el nombre del observador y cuenta los registros;
+SELECT observer, COUNT(*) AS total_observations
+FROM observations
+GROUP BY observer
+ORDER BY total_observations DESC;
+
 
 -- 10) Muestra el nombre de la región (regions.name) para cada observación;
 -- Relaciona observations con regions usando region_id;
+SELECT o.id, o.observer, r.name AS region_name
+FROM observations o
+INNER JOIN regions r
+ON o.region_id = r.id;
+
+
+SELECT * FROM observations
+INNER JOIN regions
+ON region_id = id;
+
+
+
 
 -- 11) Muestra el nombre científico de cada especie registrada (species.scientific_name);
 -- Relaciona observations con species usando species_id;
